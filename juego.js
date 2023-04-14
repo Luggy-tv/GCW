fb2807ec81d99f951bdbb4a7221d53e2c6afb374
Luis Alfonso Martinez Martinez <luisalfonsomartinezmartinez@outlook.es>
2023-04-14 02:51:06 -0600

    Modelos

---
+++ juego.js
@@ -0,0 +1,145 @@
+import * as THREE from "./modules/three.module.js";
+import { OrbitControls } from "./modules/OrbitControls.js";
+import { FBXLoader } from "./modules/FBXLoader.js";
+
+//Scene
+THREE.ColorManagement.enabled = true;
+const scene = new THREE.Scene();
+scene.background = new THREE.Color("#34495E");
+//Camera-Camara
+const camera = new THREE.PerspectiveCamera(
+  60,
+  window.innerWidth / window.innerHeight
+);
+camera.position.set(0, 95, 110);
+//Renderer
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const directionalLight = new THREE.DirectionalLight(0xffffbb, 2);
+directionalLight.position.set(10, 1, 8);
+scene.add(directionalLight);
+
+const directionalLight2 = new THREE.DirectionalLight(0xffffbb, 2);
+directionalLight2.position.set(-10, 1, -8);
+scene.add(directionalLight2);
+
+//OrbitControls
+const cameraControl = new OrbitControls(camera, renderer.domElement);
+
+//Cubo
+// const cube1Geometry = new THREE.BoxGeometry(1, 1, 1);
+// const cube1Material = new THREE.MeshBasicMaterial({ color: "aqua" });
+// const cube1 = new THREE.Mesh(cube1Geometry, cube1Material);
+// scene.add(cube1);
+
+//Escenario
+
+const escenario = new FBXLoader();
+escenario.load(
+  "./Recursos/Modelos/escenario.fbx",
+  function (object) {
+    // El objeto FBX se ha cargado exitosamente
+    // Puedes agregarlo a tu escena Three.js aquí
+    scene.add(object);
+  },
+  function (xhr) {
+    // Función de progreso opcional
+    console.log((xhr.loaded / xhr.total) * 100 + "% cargado");
+  },
+  function (error) {
+    // Función de manejo de errores opcional
+    console.error("Error al cargar el archivo FBX", error);
+  }
+);
+
+//Pusher1
+const pusher1 = new FBXLoader();
+let pusher1object;
+pusher1.load(
+  "./Recursos/Modelos/pusher1.fbx",
+  function (object) {
+    pusher1object = object;
+    scene.add(pusher1object);
+  },
+  function (xhr) {
+    console.log((xhr.loaded / xhr.total) * 100 + "% cargado");
+  },
+  function (error) {
+    console.error("Error al cargar el archivo FBX", error);
+  }
+);
+
+//Pusher2
+const pusher2 = new FBXLoader();
+let pusher2object;
+pusher2.load(
+  "./Recursos/Modelos/pusher2.fbx",
+  function (object) {
+    pusher2object = object;
+    scene.add(pusher2object);
+  },
+  function (xhr) {
+    console.log((xhr.loaded / xhr.total) * 100 + "% cargado");
+  },
+  function (error) {
+    console.error("Error al cargar el archivo FBX", error);
+  }
+);
+
+//Puck
+const puck = new FBXLoader();
+let puckObject;
+puck.load(
+  "./Recursos/Modelos/puck.fbx",
+  function (object) {
+    puckObject = object;
+    scene.add(puckObject);
+  },
+  function (xhr) {
+    console.log((xhr.loaded / xhr.total) * 100 + "% cargado");
+  },
+  function (error) {
+    console.error("Error al cargar el archivo FBX", error);
+  }
+);
+
+document.onkeydown = function (e) {
+  //console.log(e);
+  if (e.code == "KeyC") {
+    console.log(camera.position);
+  }
+
+  if (e.code == "ArrowRight") {
+    pusher1object.position.x += 2;
+  }
+  if (e.code == "ArrowLeft") {
+    pusher1object.position.x -= 2;
+  }
+  if (e.code == "ArrowUp") {
+    pusher1object.position.z -= 2;
+  }
+  if (e.code == "ArrowDown") {
+    pusher1object.position.z += 2;
+  }
+
+  if (e.code == "KeyW") {
+    pusher2object.position.z -= 2;
+  }
+  if (e.code == "KeyS") {
+    pusher2object.position.z += 2;
+  }
+  if (e.code == "KeyA") {
+    pusher2object.position.x -= 2;
+  }
+  if (e.code == "KeyD") {
+    pusher2object.position.x += 2;
+  }
+};
+
+function animate() {
+  renderer.render(scene, camera);
+  requestAnimationFrame(animate);
+}
+animate();
